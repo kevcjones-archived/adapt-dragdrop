9f7efb9f0ea782c8b8820420d4d5515f346f0c86
KevCJones <him@technicalsamurai.com>
2014-07-10 17:06:32 +0100

    first commit

---
+++ tests/spec/callout.js
@@ -0,0 +1,23 @@
+define(['../libraries/AdaptMock.js', 'jquery', 'contextFactory'], function(AdaptMock, $, contextFactory) {
+
+    var createContext = function () {
+            return contextFactory({
+                'coreJS/adapt': AdaptMock.create(),
+                'coreViews/componentView': { extend: AdaptMock.stub() },
+                'jquery': $
+            });
+        };
+
+    describe('Callout Component ...', function() {
+
+        it('should register', function(done) {
+            var context = createContext();
+            context(['coreJS/adapt', 'adapt-contrib-callout'], function (Adapt, plugin) {
+                expect(Adapt.register.called).to.be(true);
+                done();
+            });
+        });
+
+    });
+
+});
