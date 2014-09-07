edcbb3290767ca2b73f1dcb371cc2e589288e07b
Kevin Jones <kevcjones@kevins-mbp.home>
2014-09-07 23:31:01 +0100

    minor edits

--- js/adapt-dragdrop.js
+++ js/adapt-dragdrop.js
@@ -24,6 +24,9 @@ define(["coreViews/questionView", "coreJS/adapt","./jquery-ui.js","./jquery.ui.t
             // IMPORTANT! 
             // Both of the following methods need to be called inside your view.
 
+            //HOOK UP
+
+
             // Use this to set the model status to ready. 
             // It should be used when telling Adapt that this view is completely loaded.
             // This is sometimes used in conjunction with imageReady.
@@ -34,20 +37,60 @@ define(["coreViews/questionView", "coreJS/adapt","./jquery-ui.js","./jquery.ui.t
             this.setCompletionStatus();
         },
 
+        resetQuestion: function(properties) {
+            QuestionView.prototype.resetQuestion.apply(this, arguments);
+
+            //TODO reset Drag and Dropper
+
+            // _.each(this.model.get('_items'), function(item) {
+            //     item.selected = false;
+            // }, this);
+        },
+
+        canSubmit: function() {
+            //TODO when we have the drag drop code in we want to disable until they play all
+            return true;
+        },
+
+        canReset: function() {
+            return !this.$('.mcq-widget, .button.reset').hasClass('disabled');
+        },
+
+        storeUserAnswer:function() {
+            //TODO set useranswer to what the user put when he pressed submit
+            this.model.set('_userAnswer', []);
+        },
+
         onResetClicked: function(event) {
-            event.preventDefault();
+            if (this.canReset()) {
+                QuestionView.prototype.onResetClicked.apply(this, arguments);
+            } else {
+                if (event) {
+                    event.preventDefault();
+                }
+            }
+        },
+
+        setResetButtonEnabled: function(enabled) {
+            this.$('.button.reset').toggleClass('disabled', !enabled);
         },
 
         onSubmitClicked: function(event) {
-           event.preventDefault();
+            QuestionView.prototype.onSubmitClicked.apply(this, arguments);
+
+            if (this.canSubmit()) {
+               this.setResetButtonEnabled(!this.model.get('_isComplete'));
+            }
         },
 
         onModelAnswerShown: function() {
             event.preventDefault();
+            //show the correct answer
         },
 
         onUserAnswerShown: function(event) {
            event.preventDefault();
+           //show the answer the user submitted
         }
         
     });
