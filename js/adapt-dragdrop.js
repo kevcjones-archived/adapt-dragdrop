c39447c97a93e09cc207aa6fdd95a5540f0211d5
KevCJones <him@technicalsamurai.com>
2014-09-10 17:36:40 +0100

    rewrite is here

--- js/adapt-dragdrop.js
+++ js/adapt-dragdrop.js
@@ -3,216 +3,162 @@
 * License - http://github.com/adaptlearning/adapt_framework/LICENSE
 * Maintainers - Kevin Jones <him@kevincjones.co.uk>
 */
+define(function(require) {
 
-define(["coreViews/questionView", "coreJS/adapt","./jquery-ui.js","./jquery.ui.touch-punch.js",'./dragdrop.js'], function(QuestionView, Adapt, JQueryUI, touchPunch,DragDropHelper) {
-
+    var Adapt           = require('coreJS/adapt');
+    var QuestionView    = require('coreViews/questionView');
+    var JQueryUI        =  require('./jquery-ui.js');
+    var TouchPunch      = require('./jquery.ui.touch-punch.js')
 
     var DragDrop = QuestionView.extend({
 
         events: {
-            "click .dragdrop-widget .button.submit": "onSubmitClicked",
-            "click .dragdrop-widget .button.reset": "onResetClicked",
-            "click .dragdrop-widget .button.model": "onModelAnswerClicked",
-            "click .dragdrop-widget .button.user": "onUserAnswerClicked"
+            "dropactivate .ui-droppable"   : "onDropItemActivate",                     //Triggered when an accepted draggable starts dragging.  function( event, ui ) {} );
+            "dropdeactivate .ui-droppable" : "onDropItemDeactivate",                   //Triggered when an accepted draggable stops dragging. $( ".selector" ).on( "dropdeactivate", function( event, ui ) {} );
+            "drop .ui-droppable"           : "onDropItem",                             //Triggered when an accepted draggable is dropped on the droppable  $( ".selector" ).on( "drop", function( event, ui ) {} );
+            "out .ui-droppable"            : "onDropOutItem",                          //Triggered when an accepted draggable is dragged out of the droppable   $( ".selector" ).on( "dropout", function( event, ui ) {} );
+            "over .ui-droppable"           : "onDropOverItem",                         //Triggered when an accepted draggable is dragged over the droppable  $( ".selector" ).on( "dropover", function( event, ui ) {} );
+
+            "click .dragdrop-widget .button.submit" : "onSubmitClicked",
+            "click .dragdrop-widget .button.reset"  : "onResetClicked",
+            "click .dragdrop-widget .button.model"  : "onModelAnswerClicked",
+            "click .dragdrop-widget .button.user"   : "onUserAnswerClicked",
+
         },
 
-        initialize:function() {
-            QuestionView.prototype.initialize.apply(this, arguments);
-
-            this.model.set('_selectedItems', []);
+        onDropItemActivate : function( event, ui ) {
+            var droppableId = $(ui.draggable.element).attr("id");
         },
 
-        preRender: function() {
+        onDropItemDeactivate : function( event, ui ) {
+            var droppableId = $(ui.draggable.element).attr("id");
+        },
 
+        onDropItem : function( event, ui ) {
+            var droppableId = $(ui.draggable.element).attr("id");
+        },
+
+        onDropOutItem : function( event, ui ) {
+            var droppableId = $(ui.draggable.element).attr("id");
+        },
+
+        onDropOverItem : function( event, ui ) {
+            var droppableId = $(ui.draggable.element).attr("id");
+        },
+
+        findDraggableItem:function(id){
+            return _.find(item._draggableItems,function(dragger){
+                if(dragger.id == id)
+                    return dragger;
+            });
+        },
+
+        preRender:function(){
             QuestionView.prototype.preRender.apply(this);
 
-            console.log("precalculating");
+            //TO DO hook up the drag and drop based on model
+        },   
 
-            var oWidth = this.model.get("_background").originalWidth;
-            var oHeight = this.model.get("_background").originalHeight;
-            var items = this.model.get("_items");
-            _.each(items,function(item){
-                item._width = ((item.width/oWidth)*100.0) +"%";
-                item._height = ((item.height/oHeight)*100.0)+"%";
-                item._left = ((item.left/oWidth)*100.0) +"%";
-                item._top = ((item.top/oHeight)*100.0)+"%";
-            });
-
-            this.listenTo(Adapt, 'device:resize', this.onScreenSizeChanged);
-
-           
-        },
-        
         postRender: function() {
             QuestionView.prototype.postRender.apply(this);
-
-            console.log("rendering");
-
-            // IMPORTANT! 
-            // Both of the following methods need to be called inside your view.
-
-            //HOOK UP
-            this.onScreenSizeChanged();
-
-            var lessAttribs = this.model.get("LESS");
-
-            DragDropHelper.Dragger.DEFAULT_BOUNDS = lessAttribs.DEFAULT_BOUNDS;//"#wrapper";
-            DragDropHelper.Dragger.DEFAULT_DRAGGING_CLASS = lessAttribs.DEFAULT_DRAGGING_CLASS;//"ddd-dragging-s1";
-            DragDropHelper.Dragger.DEFAULT_PLACED_CLASS = lessAttribs.DEFAULT_PLACED_CLASS;//"ddd-placed-s1";
-
-            DragDropHelper.DropZone.DEFAULT_NEUTRAL_CLASS =  lessAttribs.DEFAULT_NEUTRAL_CLASS;//"neutraldropzone-s1";
-            DragDropHelper.DropZone.DEFAULT_CORRECT_CLASS =  lessAttribs.DEFAULT_CORRECT_CLASS;//"correctdropzone-s1";
-            DragDropHelper.DropZone.DEFAULT_INCORRECT_CLASS =  lessAttribs.DEFAULT_INCORRECT_CLASS;//"incorrectdropzone-s1";
-
-            DragDropHelper.ControllerFactory.INACTIVE_BUTTON_CLASS = lessAttribs.INACTIVE_BUTTON_CLASS;//"inactive-but";
-
-            var masterDragList = [];
-            var draggerDictionary = {};
-            var masterDropList = [];
-
-            this.model.set("_isComplete",false);
-
-            var items = this.model.get("_items");
-            _.each(items,function(item){
-                if(item.type == "dragger")
-                {
-                    masterDragList.push(DragDropHelper.DraggerFactory({tgtId:'#'+item.id}));
-                    draggerDictionary[item.id] = masterDragList[masterDragList.length-1];
-                }
-            });
-
-
-            _.each(items,function(item){
-                if(item.type == "dropzone")
-                {
-                    item.correctDraggers = [];
-
-                    _.each(item.correct,function(correct){
-                        item.correctDraggers.push(draggerDictionary[correct]);
-                    });
-
-                    masterDropList.push(DragDropHelper.DropZoneFactory({
-                        targetId:'#'+item.id,
-                        acceptedDraggers : masterDragList,
-                        correctDraggers : item.correctDraggers
-                    }));
-                }
-            });
-
-            
-
-            this.ddController = DragDropHelper.ControllerFactory({
-                $resetButton : null,
-                $revealButton: null,
-                $submitButton: null,
-                dropzones :masterDropList,
-                draggers: masterDragList
-            });     
-
-            // Use this to set the model status to ready. 
-            // It should be used when telling Adapt that this view is completely loaded.
-            // This is sometimes used in conjunction with imageReady.
             this.setReadyStatus();
+        },   
 
-            // Use this to set the model status to complete.
-            // This can be used with inview or when the model is set to complete/the question has been answered.
-            this.setCompletionStatus();
-        },
-
-        onScreenSizeChanged: function() {
-            // if (Adapt.device.screenSize != 'large') {
-            //     this.replaceWithNarrative();
-            // }
-            var h = $(this.el).find('.dragdrop-background').height();
-            $(this.el).find('.dragdrop-container').css("height",h+"px");
-
-            var oWidth = this.model.get("_background").originalWidth;
-            var w = $(this.el).find('.dragdrop-background').width();
-            var scale = (100.0*(w/oWidth))+'%';
-            $(this.el).find('.dragdrop-container').css("font-size",scale);
-
-
-
-        },
-
-
+        /*
+        Look at all droppables and if one does not contain a highlight class then its fair to assume we have not completed the excercise
+        */
         canSubmit: function() {
-            //TODO when we have the drag drop code in we want to disable until they play all
-            return this.ddController.canSubmit();
-        },
-
-        canReset: function() {
-            return !this.$('.mcq-widget, .button.reset').hasClass('disabled');
-        },
-
-
-        storeUserAnswer:function() {
-            //TODO set useranswer to what the user put when he pressed submit
-            this.model.set('_userAnswer', []);
-        },
-
-        onResetClicked: function(event) {
-            if (this.canReset()) {
-                QuestionView.prototype.onResetClicked.apply(this, arguments);
-                this.ddController.reset();
-            } else {
-                if (event) {
-                    event.preventDefault();
-                }
-            }
+          var canSubmit = true;
+          $('.ui-droppable',this.el).each(
+            _.bind(function(index, element) {
+              var $element = $(element);
+              if (!$element.hasClass('ui-state-highlight')) {
+                canSubmit = false;
+              }
+            }, this));
+          return canSubmit;
         },
 
         forEachAnswer: function(callback) {
             this.model.set('_isAtLeastOneCorrectSelection', false);
-            _.each(this.model.get('_items'), function(item, index) {
-                if(item.type != "dropzone") return;
-                var correctSelection = this.ddController.draggerCorrect(item.id);
-                if(correctSelection) {
-                    this.model.set('_isAtLeastOneCorrectSelection', true);
-                }
+            _.each(this.model.get('_items'), function(item, index) 
+            {       
+                var correctSelection = false;
+                _.each(item._accepted,function(accepted){
+                    if(item.currentDropzoneId == accepted.id)
+                    {
+                        if(accepted._isCorrect){
+                            correctSelection = true;
+                            item._isCorrect = true;
+                            this.model.set('_isAtLeastOneCorrectSelection', true);
+                        }else
+                        {
+                            item._isCorrect = false;
+                        }
+                    }
+                });
+               
                 callback(correctSelection, item);
+
             }, this);
-        },
+        },  
 
         markQuestion: function() {
             this.forEachAnswer(function(correct, item) {
                 item.correct = correct;
             });
             QuestionView.prototype.markQuestion.apply(this);
-        },
+        },  
 
-        setResetButtonEnabled: function(enabled) {
-            this.$('.button.reset').toggleClass('disabled', !enabled);
-        },
-
-        onSubmitClicked: function(event) {
-
-            if (this.canSubmit()) {
-               this.ddController.submit();
-                QuestionView.prototype.onSubmitClicked.apply(this, arguments);
-
-               this.model.set('_numberOfCorrectAnswers',this.ddController.correctItems());
-               this.setResetButtonEnabled(!this.model.get('_isComplete'));
-            }
-        },
-
-        isCorrect: function() {
-            return !!Math.floor(this.model.get('_numberOfCorrectAnswers') / this.ddController.allZones.length);
-        },
-        
-        isPartlyCorrect: function() {
-            return !this.isCorrect() && this.model.get('_isAtLeastOneCorrectSelection');
+        onEnabledChanged: function() {
+          this.$('.ui-droppable').prop('disabled', !this.model.get('_isEnabled'));
         },
 
         onModelAnswerShown: function() {
-            event.preventDefault();
-            //show the correct answer
+            /*
+                for all items get jquery item on the deck
+                    if said item is correct leave alone
+                    if said item is incorrect 
+                        revert the dropper in it
+                        and tell the dropper that it should be to go to it
+            */
+            alert("TODO : SHOW the model answer");
+
+            _.each(this.model.get('_items'), function(item, index) {
+
+            }, this);
         },
 
         onUserAnswerShown: function(event) {
-           event.preventDefault();
-           //show the answer the user submitted
+            /*
+                for the items array get the item and index
+                    get the item _useranswer array at index
+                    with the item 
+                        if incorrect 
+                            revert
+                            select correct item by _userAnswerId and animate into position
+                        else
+                            ignore
+
+            */
+            _.each(this.model.get('_items'), function(item, index) 
+            {
+                //this.model.get('_userAnswer')[i]                
+            },this);
+        },
+
+        storeUserAnswer: function() {
+            var userAnswer = [];
+            
+            this.forEachAnswer(function(correct, item) {
+                userAnswer.push(item.currentDropzoneId);
+            });
+
+            this.model.set('_userAnswer', userAnswer);
         }
+
+
+        
         
     });
     
